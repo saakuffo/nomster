9359d133758fdccd2b4da123cc64d3a418605a96
Stephen Akuffo <slyven@gmail.com>
2016-06-17 02:40:24 +0000

    built submit new place feature

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,19 +1,20 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
+    @places = Place.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
     @place = Place.new
   end
 
-  # def create
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
 
-  # end
+  private
 
-  # private
-
-  # def params
-    
-  # end
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
