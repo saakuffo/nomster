b24d6054953be925ccf89a38cf1fe9f8c29ebdcf
Stephen Akuffo <slyven@gmail.com>
2016-06-20 15:07:48 +0000

    added comments model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+
   validates :user, presence: true
   validates :name, length: {minimum: 3}, presence: true, uniqueness: { case_sensitive: false}
   validates :address, presence: true
