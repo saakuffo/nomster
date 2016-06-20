16b81b16f4dbb8a73f02200f5ffd00c0141d70cc
Stephen Akuffo <slyven@gmail.com>
2016-06-20 04:00:45 +0000

    geocoder added

--- Gemfile
+++ Gemfile
@@ -37,6 +37,10 @@ gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
 gem 'simple_form'
 gem 'devise', '>= 3.2.4'
 
+gem 'geocoder', "1.3.4"
+
+gem 'figaro', ">= 1.0.0"
+
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  validates :user, presence: true
+  validates :name, length: {minimum: 3}, presence: true, uniqueness: { case_sensitive: false}
+  validates :address, presence: true
+  validates :description, presence: true
 end
