f8458ae1e7d47d2195f0a1ae3107156d59127785
Stephen Akuffo <slyven@gmail.com>
2016-06-23 04:03:28 +0000

    user can only see own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def show
     # @user = current_user
     @user = User.find(params[:id])
+
+    if @user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 end
