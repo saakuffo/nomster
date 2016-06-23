10006ecde21dfd24b669285d63721f7226f67b34
Stephen Akuffo <slyven@gmail.com>
2016-06-23 03:55:17 +0000

    spec complete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+    @places = Place.all.order('created_at DESC').paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -49,7 +49,7 @@ class PlacesController < ApplicationController
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render text:'Not Allowed', status: :forbidden
+      return render text: 'Not Allowed', status: :forbidden
     end
     @place.destroy
     redirect_to root_path
