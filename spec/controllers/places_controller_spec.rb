c0fe2dac63458224c74b3a60624c400683146d16
Stephen Akuffo <slyven@gmail.com>
2016-11-18 21:01:19 +0000

    codecov exlcude config

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe PlacesController do
   describe 'places#index actions' do
-    it 'if there is a quote in the DB go to index' do
+    it 'should load the home page' do
       get :index
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'places#new actions' do
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
     end
   end
 end
